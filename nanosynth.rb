34c22c13bf3328e0ef437f872cf452eea0d19462
Joel Strait <joel.strait@gmail.com>
2019-12-29 18:24:50 -0800

    Removing extra whitespace

--- nanosynth.rb
+++ nanosynth.rb
@@ -47,10 +47,10 @@ RANDOM_GENERATOR = Random.new
 
 def main
   # Read the command-line arguments.
-  waveform = ARGV[0].to_sym     # Should be "sine", "square", "saw", "triangle", or "noise"
-  frequency = ARGV[1].to_f      # 440.0 is the same as middle-A on a piano.
-  amplitude = ARGV[2].to_f      # Should be between 0.0 (silence) and 1.0 (full volume).
-                                # Amplitudes above 1.0 will result in distortion (or other weirdness).
+  waveform = ARGV[0].to_sym  # Should be "sine", "square", "saw", "triangle", or "noise"
+  frequency = ARGV[1].to_f   # 440.0 is the same as middle-A on a piano.
+  amplitude = ARGV[2].to_f   # Should be between 0.0 (silence) and 1.0 (full volume).
+                             # Amplitudes above 1.0 will result in clipping distortion.
 
   # Generate sample data at the given frequency and amplitude.
   # The sample rate indicates how many samples we need to generate for
